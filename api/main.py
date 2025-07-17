ec725dfdb2aa407157d69d7bc5a6e64a1818124e
Patrik Barkman <patrik.barkman@volumental.com>
2025-07-17 11:02:02 +0200

    Read relay url from environment variable

--- api/main.py
+++ api/main.py
@@ -1,14 +1,19 @@
 import httpx
 from fastapi import FastAPI, HTTPException, Response
+from pydantic_settings import BaseSettings
 
-RELAY_URL = "http://127.0.0.1:5000"
 
+class Settings(BaseSettings):
+    relay_url: str = "http://127.0.0.1:5000"
+
+
+settings = Settings()
 app = FastAPI()
 
 
 @app.get("/hls/{name}/{path}")
 async def get_hls_stream(name: str, path: str) -> Response:
-    device_url = f"{RELAY_URL}/hls/{name}/{path}"
+    device_url = f"{settings.relay_url}/hls/{name}/{path}"
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(device_url)
