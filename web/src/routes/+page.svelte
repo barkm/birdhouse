96d89a9a5488a9192130d106695e20ba3935cd4a
Patrik Barkman <patrik.barkman@volumental.com>
2025-07-27 12:25:10 +0200

    Read api url from environment

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import HLSPlayer from '$lib/HLSPlayer.svelte';
-  const streamUrl: string = 'http://localhost:8002/birdhouse/hls/playlist.m3u8';
+  import { PUBLIC_API_URL } from '$env/static/public';
 </script>
 
-<HLSPlayer src={streamUrl} />
+<HLSPlayer src={PUBLIC_API_URL} />
