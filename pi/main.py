a8f7ede538a1fea8f1f51785e5414d931bf39de4
Patrik Barkman <patrik.barkman@volumental.com>
2025-08-23 09:55:49 +0200

    Do not set cache control on segments

--- pi/main.py
+++ pi/main.py
@@ -44,11 +44,15 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/hls/{filename:path}")
 async def serve_hls_files(request: Request, filename: str):
-    headers = {"Cache-Control": "no-store", "Pragma": "no-cache", "Expires": "0"}
     stream: Stream = request.app.state.stream
     stream_path = stream.get_file(filename)
     if not stream_path:
         raise HTTPException(status_code=404, detail="File not found")
+    headers = (
+        {"Cache-Control": "no-store", "Pragma": "no-cache", "Expires": "0"}
+        if "m3u8" in filename
+        else {}
+    )
     return FileResponse(stream_path, headers=headers)
 
 
