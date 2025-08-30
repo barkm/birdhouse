a6374e46bc905fed930bd8a736fe2dada12fd129
Patrik Barkman <patrik.barkman@volumental.com>
2025-08-30 09:09:31 +0200

    Add relay install script

---
+++ relay/relay.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVICE_NAME=relay
+UV=$(which uv)
+
+if [[ "$1" == "install" ]]; then
+  sudo $UV run service.py install --name $SERVICE_NAME --command "$(which uv) run fastapi run main.py"
+elif [[ "$1" == "uninstall" ]]; then
+  sudo $UV run service.py uninstall --name $SERVICE_NAME 
+else
+  echo "Usage: $0 <install|uninstall>"
+  exit 1
+fi
---
+++ relay/service.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+from subprocess import check_call
+import getpass
+import os
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--name", required=True)
+@click.option("--command", required=True)
+@click.option("environment_variables", "--environment", multiple=True, default=[])
+def install(name: str, command: str, environment_variables: list[str]):
+    _install(name, command, environment_variables)
+
+
+def _install(name: str, command: str, environment_variables: list[str]):
+    _write_service_file(name, command, environment_variables)
+    _enable_service(name)
+    _start_service(name)
+
+
+@cli.command()
+@click.option("--name", required=True)
+def uninstall(name: str):
+    _uninstall(name)
+
+
+def _uninstall(name: str):
+    _stop_service(name)
+    _disable_service(name)
+    _remove_service_file(name)
+
+
+def _write_service_file(
+    name: str, command: str, environment_variables: list[str]
+) -> None:
+    file_path = _get_service_file_path(name)
+    content = _get_service_file_content(command, environment_variables)
+    if file_path.exists():
+        _uninstall(name)
+    file_path.write_text(content)
+
+
+def _remove_service_file(name: str) -> None:
+    _get_service_file_path(name).unlink()
+
+
+def _get_service_file_content(command: str, environment_variables: list[str]) -> str:
+    env_lines = "\n".join(f"Environment={var}" for var in environment_variables)
+    return f"""
+[Unit]
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+User={os.environ.get("SUDO_USER") or getpass.getuser()}
+WorkingDirectory={Path.cwd()}
+{env_lines}
+ExecStart={command}
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+    """
+
+
+def _get_service_file_path(name: str) -> Path:
+    return Path(f"/etc/systemd/system/{name}.service")
+
+
+def _enable_service(name: str) -> None:
+    _run_systemctl_command("enable", name)
+
+
+def _disable_service(name: str) -> None:
+    _run_systemctl_command("disable", name)
+
+
+def _start_service(name: str) -> None:
+    _run_systemctl_command("start", name)
+
+
+def _stop_service(name: str) -> None:
+    _run_systemctl_command("stop", name)
+
+
+def _run_systemctl_command(command: str, service_name: str) -> None:
+    check_call(["systemctl", command, service_name])
+
+
+if __name__ == "__main__":
+    cli()
