a913ba1c2cb38530f8d99d7568f4c72098d40ac9
Patrik Barkman <patrik.barkman@volumental.com>
2025-08-30 16:22:07 +0200

    Break out fetching src from HLSPlayer

--- web/src/lib/HLSPlayer.svelte
+++ web/src/lib/HLSPlayer.svelte
@@ -2,29 +2,20 @@
   import { onMount, onDestroy } from 'svelte';
   import Hls from 'hls.js';
 
-  const { relay_url } = $props<{ relay_url: string }>();
+  const { src } = $props<{ src: string }>();
 
   let videoElement: HTMLVideoElement;
   let hls: Hls | null = null;
 
-  const playlist_path_promise = $derived(
-    fetch(`${relay_url}birdhouse/start`).then((res) => res.json()).then((data): string | undefined => data.playlist)
-  )
 
   onMount(() => {
-    playlist_path_promise.then((playlist_path?: string) => {
-      if (!playlist_path) {
-        console.log('No playlist found');
-        return
-      }
       if (Hls.isSupported()) {
         hls = new Hls();
-        hls.loadSource(`${relay_url}birdhouse${playlist_path}`);
+        hls.loadSource(src);
         hls.attachMedia(videoElement);
       } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
-        videoElement.src = `${relay_url}birdhouse${playlist_path}`;
+        videoElement.src = src;
       }
-    });
 
     return () => {
       if (hls) {
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import HLSPlayer from '$lib/HLSPlayer.svelte';
   import { PUBLIC_RELAY_URL } from '$env/static/public';
+  
+  const playlist_path_promise = $derived(
+    fetch(`${PUBLIC_RELAY_URL}birdhouse/start`).then((res) => res.json()).then((data): string => data.playlist)
+  )
 </script>
 
-<HLSPlayer relay_url={PUBLIC_RELAY_URL} />
+{#await playlist_path_promise then playlist_path}
+  <HLSPlayer src={`${PUBLIC_RELAY_URL}birdhouse${playlist_path}`} />
+{/await}
