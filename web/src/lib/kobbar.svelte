9f4c0d54262dc6326003f2f30bf696a4195c8a03
Patrik Barkman <patrik.barkman@volumental.com>
2025-08-31 17:39:40 +0200

    Animate loader

--- web/src/lib/kobbar.svelte
+++ web/src/lib/kobbar.svelte
@@ -20,19 +20,22 @@
          style="display:inline"
          >
         <path
-           style="fill:none;stroke:#000000;stroke-width:1.82544;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-           d="m 98.957565,116.33307 h 66.078955 0.23105"
-           id="path916"
-           />
-        <path
+           class="reflection"
            id="path916-4"
            d="m 114.98034,122.75939 h 35.94022 0.12557"
            style="display:inline;fill:none;stroke:#000000;stroke-width:1.82544;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
         <path
+           class="reflection"
            style="display:inline;fill:none;stroke:#000000;stroke-width:1.82544;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
            d="m 122.42957,129.74119 h 17.72751 0.062"
            id="path916-4-5"
 />
+        <path
+           class="reflection"
+           style="fill:none;stroke:#000000;stroke-width:1.82544;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+           d="m 98.957565,116.33307 h 66.078955 0.23105"
+           id="path916"
+           />
       </g>
     </g>
     <g
@@ -40,21 +43,25 @@
        class="birds"
        transform="matrix(1.0657862,0,0,1.1127754,-9.0430955,-25.35503)">
       <path
+         class="bird"
          style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.483744;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
          d="m 201.35717,32.784689 c 6.00488,-0.184847 8.54495,0.266028 11.66474,5.995588 4.85145,-3.923438 8.58865,-5.913919 14.19535,-4.943594 -6.43859,-3.540485 -9.33501,-1.29815 -12.74722,1.052608 -2.68613,-3.831162 -8.57437,-6.396182 -13.11287,-2.104602 z"
          id="path982"
          />
       <path
+         class="bird"
          id="path982-1"
          d="m 175.21146,54.657001 c 6.16788,-0.650242 11.47921,1.351944 13.13185,8.477682 5.5035,-7.64658 12.50097,-7.620358 18.10766,-3.702548 -4.31941,-6.48797 -12.59525,-4.866159 -16.00747,-2.515401 -1.87107,-4.296555 -10.69353,-6.551311 -15.23204,-2.259733 z"
          style="display:inline;fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.48374;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
          />
       <path
+         class="bird"
          id="path982-9"
          d="m 205.26966,47.189665 c 7.47199,-1.270763 10.1751,0.88655 12.31679,7.236635 6.15557,-6.715794 9.4037,-4.983135 15.01041,-2.151239 -2.85227,-7.108494 -9.66103,-5.641814 -13.07324,-3.291057 -1.87108,-4.296553 -9.71545,-6.085917 -14.25396,-1.794339 z"
          style="display:inline;fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.48374;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
          />
       <path
+         class="bird"
          id="path982-8"
          d="m 168.94977,40.607769 c 6.00488,-0.184846 10.33811,-0.509626 14.10993,5.064805 4.85146,-3.92344 8.75167,-5.758789 14.35836,-4.788463 -6.43858,-3.540486 -10.15005,-1.14302 -13.56227,1.207738 -2.68613,-3.831162 -9.38943,-4.844872 -14.90602,-1.48408 z"
          style="display:inline;fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.48374;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
