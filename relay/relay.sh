c66a2465d578b8f84d82b693c72b6ca20919e769
Patrik Barkman <patrik.barkman@volumental.com>
2025-09-08 22:14:57 +0200

    Set allowed emails in relay service install

--- relay/relay.sh
+++ relay/relay.sh
@@ -4,7 +4,10 @@ SERVICE_NAME=relay
 UV=$(which uv)
 
 if [[ "$1" == "install" ]]; then
-  sudo $UV run service.py install --name $SERVICE_NAME --command "$(which uv) run fastapi run main.py --port 5000"
+  sudo $UV run service.py install \
+    --name $SERVICE_NAME \
+    --environment ALLOWED_EMAILS=$ALLOWED_EMAILS \
+    --command "$(which uv) run fastapi run main.py --port 5000"
 elif [[ "$1" == "uninstall" ]]; then
   sudo $UV run service.py uninstall --name $SERVICE_NAME 
 else
