d49149f2cd9a346d35aa8dc399213a31f3abb270
Patrik Barkman <patrik.barkman@volumental.com>
2025-09-13 11:15:44 +0200

    Remove allowed emails from relay deploy

--- relay/relay.sh
+++ relay/relay.sh
@@ -6,7 +6,6 @@ UV=$(which uv)
 if [[ "$1" == "install" ]]; then
   sudo $UV run service install \
     --name $SERVICE_NAME \
-    --environment ALLOWED_EMAILS=$ALLOWED_EMAILS \
     --command "$(which uv) run fastapi run main.py --port 5000"
 elif [[ "$1" == "uninstall" ]]; then
   sudo $UV run service uninstall --name $SERVICE_NAME 
