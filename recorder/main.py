85fdcd71742724b364c2ec339fb095f0b2a1dc09
Patrik Barkman <patrik.barkman@volumental.com>
2025-10-19 09:43:28 +0200

    Add timestamp label

--- recorder/main.py
+++ recorder/main.py
@@ -1,4 +1,5 @@
 from contextlib import asynccontextmanager
+from itertools import zip_longest
 from pathlib import Path
 from datetime import datetime
 from subprocess import CalledProcessError, run
@@ -14,8 +15,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel
 from google.cloud import storage
-from moviepy import VideoFileClip, CompositeVideoClip
-from moviepy.video.fx import CrossFadeIn
+from moviepy import VideoFileClip, CompositeVideoClip, TextClip
+from moviepy.video.fx import CrossFadeIn, CrossFadeOut
 
 from common.auth import firebase
 from common.auth import google
@@ -227,13 +228,27 @@ def _make_timelapse(
     total_time: int | None = None,
     fade_duration: int | None = None,
 ) -> None:
+    fade_duration = fade_duration or 0
     recordings = _list_gcs_recordings(f"{settings.recording_dir}/{device}")
     recordings_in_range = [
         r for r in recordings if start <= datetime.fromisoformat(r.time) <= end
     ]
     downloaded_files = [_download_recording(r.url) for r in recordings_in_range]
     times = [datetime.fromisoformat(r.time) for r in recordings_in_range]
-    clips = [VideoFileClip(str(f)) for f in downloaded_files]
+    clips = [VideoFileClip(str(f)).with_speed_scaled(2) for f in downloaded_files]
+    text_clips = [
+        TextClip(
+            font="Arial",
+            text=t.strftime("%Y-%m-%d %H:%M:%S"),
+            size=(200, 100),
+            color="white",
+            text_align="center",
+            horizontal_align="center",
+        )
+        .with_position((0.75, 0.9), relative=True)
+        .with_duration(c.duration)
+        for t, c in zip(times, clips)
+    ]
     optional_durations = [clip.duration for clip in clips]
     if any(d is None for d in optional_durations):
         raise ValueError("Could not get duration of all clips")
@@ -244,7 +259,7 @@ def _make_timelapse(
             times[i],
             durations[i],
             times[i + 1],
-            fade_duration or 0,
+            fade_duration,
         )
         for i in range(len(durations) - 1)
     )
@@ -261,14 +276,33 @@ def _make_timelapse(
 
     starts = [compression * (t - times[0]).total_seconds() for t in times]
 
-    if fade_duration is not None:
-        clips = [clips[0]] + [
-            c.with_effects([CrossFadeIn(fade_duration)]) for c in clips[1:]
-        ]
+    clips = [
+        clip.with_start(start).subclipped(0, (end + fade_duration) if end else None)
+        for clip, start, end in zip_longest(clips, starts, starts[1:])
+    ]
+    text_clips = [
+        tc.with_start(start).subclipped(0, (end + fade_duration) if end else None)
+        for tc, start, end in zip_longest(text_clips, starts, starts[1:])
+    ]
 
-    timelapse = CompositeVideoClip(
-        [clip.with_start(start) for clip, start in zip(clips, starts)]
-    )
+    if fade_duration is not None:
+        clips = (
+            [clips[0].with_effects([CrossFadeOut(fade_duration)])]
+            + [c.with_effects([CrossFadeIn(fade_duration)]) for c in clips[1:-1]]
+            + [clips[-1].with_effects([CrossFadeIn(fade_duration)])]
+        )
+        text_clips = (
+            [text_clips[0].with_effects([CrossFadeOut(fade_duration)])]
+            + [
+                tc.with_effects(
+                    [CrossFadeIn(fade_duration), CrossFadeOut(fade_duration)]
+                )
+                for tc in text_clips[1:-1]
+            ]
+            + [text_clips[-1].with_effects([CrossFadeIn(fade_duration)])]
+        )
+
+    timelapse = CompositeVideoClip(clips + text_clips)
     timelapse.write_videofile(dest)
 
 
