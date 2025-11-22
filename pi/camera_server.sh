abe866a72ae0341a395fa208c49a6a79614d51dc
Patrik Barkman <patrik.barkman@volumental.com>
2025-11-22 20:32:35 +0100

    Include sensor dependencies when running camera service

--- pi/camera_server.sh
+++ pi/camera_server.sh
@@ -4,7 +4,7 @@ SERVICE_NAME=camera-server
 UV=$(which uv)
 
 if [[ "$1" == "install" ]]; then
-  sudo $UV run service install --name $SERVICE_NAME --command "$(which uv) run fastapi run main.py"
+  sudo $UV run service install --name $SERVICE_NAME --command "$(which uv) run --group sensor fastapi run main.py"
 elif [[ "$1" == "uninstall" ]]; then
   sudo $UV run service uninstall --name $SERVICE_NAME 
 else
