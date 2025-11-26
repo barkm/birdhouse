c0aaf050e41b87c90c5213fb1d32b0fa213c3bbd
Patrik Barkman <patrik.barkman@volumental.com>
2025-11-26 21:07:12 +0100

    Add register table

--- common/src/common/db/models.py
+++ common/src/common/db/models.py
@@ -1,7 +1,17 @@
 import uuid
 from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
 
 
 class Device(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str
+
+
+class Register(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc), nullable=False
+    )
+    device_id: uuid.UUID = Field(foreign_key="device.id")
+    url: str
