9b3a228e97658c1b657156c440919dc83222c2ea
Patrik Barkman <patrik.barkman@volumental.com>
2025-11-26 21:40:25 +0100

    Update tables for postgres features

--- common/src/common/db/models.py
+++ common/src/common/db/models.py
@@ -1,17 +1,19 @@
 import uuid
-from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, DateTime
 
 
 class Device(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    name: str
+    name: str = Field(index=True, unique=True)
 
 
 class Register(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     created_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc), nullable=False
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(DateTime(timezone=True), nullable=False),
     )
     device_id: uuid.UUID = Field(foreign_key="device.id")
     url: str
