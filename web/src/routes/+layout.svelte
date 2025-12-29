62b1f0294daecd0b32c3ca3e8849ce362003cec7
Patrik Barkman <patrik.barkman@volumental.com>
2025-12-29 17:10:28 +0100

    Move auth to layout

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
 	import './layout.css';
+	import { loginWithGoogle, logout, user } from "$lib/firebase";
+	import Loader from '$lib/components/loader/Loader.svelte';
 
 	const { children } = $props();
 </script>
 
-{@render children()}
+{#if $user}
+	{@render children()}
+	<div class="grid place-items-center">
+		<button class="border border-black rounded px-4 py-1 hover:bg-gray-100" onclick={logout}>Logga ut</button>
+	</div>
+{:else}
+<div class="h-screen flex flex-col justify-center items-center gap-4">
+    
+    <div class="w-3/4 max-w-sm">
+        <Loader />
+    </div>
+
+    <button 
+        class="border border-black rounded px-4 py-1 hover:bg-gray-100 transition" 
+        onclick={loginWithGoogle}
+    >
+        Logga in
+    </button>
+
+</div>
+{/if}
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,62 +1,49 @@
 <script lang="ts">
 	import { PUBLIC_RELAY_URL } from '$env/static/public';
 	import Device from '$lib/components/Device.svelte';
-	import { loginWithGoogle, logout, user } from '$lib/firebase';
-	import Loader from '$lib/components/loader/Loader.svelte';
 	import { authorizedRequest } from '$lib/request';
 	import TemperatureChart from '$lib/components/TemperatureChart.svelte';
+	import { user } from '$lib/firebase';
 
 	interface Device {
 		name: string;
 	}
 
 	const fetch_devices = async (): Promise<Device[]> => {
-		if (!$user) return [];
-		const device_response = await authorizedRequest($user, PUBLIC_RELAY_URL, 'list');
+		const device_response = await authorizedRequest($user!, PUBLIC_RELAY_URL, 'list');
 		return await device_response.json();
 	};
 
 	const getTemperature = async (device_name: string): Promise<number | null> => {
-		if (!$user) return null;
-		const sensor_response = await authorizedRequest($user, PUBLIC_RELAY_URL, `${device_name}/sensor`);
+		const sensor_response = await authorizedRequest($user!, PUBLIC_RELAY_URL, `${device_name}/sensor`);
 		return (await sensor_response.json())['temperature'];
 	};
 
 	const devices_promise = $derived(fetch_devices());
 </script>
 
-{#if $user}
-	<column style="width: 100%">
-		{#await devices_promise then devices}
-			<devices>
-				{#each devices as device (device.name)}
-					{#if device.name.includes("house")}
-						{#await getTemperature(device.name) then temperature}
-						<div>
-							{ device.name === "house" ? "Inomhus" : "Utomhus" }temperatur: {temperature !== null ? temperature.toFixed(1) : 'N/A'}°C
-						</div>
-						{/await}
-					{/if}
-				{/each}
-				<br />
-				<TemperatureChart {devices} />
-				{#each devices as device (device.name)}
-					{#if device.name === "birdhouse"}
-						<Device {device} />
-					{/if}
-				{/each}
-			</devices>
-		{/await}
-		<button onclick={logout}>Logga ut</button>
-	</column>
-{:else}
-	<row>
-		<column style="width: 25%">
-			<Loader />
-			<button onclick={loginWithGoogle}>Logga in</button>
-		</column>
-	</row>
-{/if}
+<column style="width: 100%">
+	{#await devices_promise then devices}
+		<devices>
+			{#each devices as device (device.name)}
+				{#if device.name.includes("house")}
+					{#await getTemperature(device.name) then temperature}
+					<div>
+						{ device.name === "house" ? "Inomhus" : "Utomhus" }temperatur: {temperature !== null ? temperature.toFixed(1) : 'N/A'}°C
+					</div>
+					{/await}
+				{/if}
+			{/each}
+			<br />
+			<TemperatureChart {devices} />
+			{#each devices as device (device.name)}
+				{#if device.name === "birdhouse"}
+					<Device {device} />
+				{/if}
+			{/each}
+		</devices>
+	{/await}
+</column>
 
 <style>
 	devices {
