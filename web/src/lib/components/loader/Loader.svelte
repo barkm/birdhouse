359d5e47959727147c9ffd853354be73fd2a97cf
Patrik Barkman <patrik.barkman@volumental.com>
2025-12-31 11:10:22 +0100

    Set loader max width on larger screens

--- web/src/lib/components/loader/Loader.svelte
+++ web/src/lib/components/loader/Loader.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="h-full w-full bg-gray-100 rounded-lg">
-	<div class="w-1/3 relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+	<div class="w-2/3 md:max-w-75 relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 		<AnimatedKobbar />
 	</div>
 </div>
