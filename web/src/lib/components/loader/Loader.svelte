bad61b1930f6cec04629af744a1ca181a9958075
Patrik Barkman <patrik.barkman@volumental.com>
2025-12-31 12:35:35 +0100

    Make kobbar color adaptable

--- web/src/lib/components/loader/Loader.svelte
+++ web/src/lib/components/loader/Loader.svelte
@@ -2,8 +2,8 @@
 	import AnimatedKobbar from './AnimatedKobbar.svelte';
 </script>
 
-<div class="h-full w-full bg-gray-100 rounded-lg">
-	<div class="w-2/3 md:max-w-75 relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+<div class="h-full w-full bg-gray-100 rounded-lg animate-pulse">
+	<div class="w-2/3 md:max-w-75 relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-300">
 		<AnimatedKobbar />
 	</div>
 </div>
