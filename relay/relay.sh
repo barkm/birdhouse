f40f10e857973c49d15a4f021812e34794ff887b
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-18 09:44:16 +0100

    Relay install takes register url

--- relay/relay.sh
+++ relay/relay.sh
@@ -4,9 +4,15 @@ SERVICE_NAME=relay
 UV=$(which uv)
 
 if [[ "$1" == "install" ]]; then
+  if [ -z "$2" ]; then
+    echo "Usage: $0 install <REGISTER_URL>"
+    exit 1
+  fi
+  REGISTER_URL="$2"
   sudo $UV run service install \
     --name $SERVICE_NAME \
-    --command "$(which uv) run fastapi run main.py --port 5000"
+    --command "$(which uv) run fastapi run main.py --port 5000" \
+    --environment "REGISTER_URL=$REGISTER_URL"
 elif [[ "$1" == "uninstall" ]]; then
   sudo $UV run service uninstall --name $SERVICE_NAME 
 else
