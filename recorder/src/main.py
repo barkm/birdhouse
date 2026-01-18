78c33232ca0d2084c5af89ab27b6dab916816aec
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-18 17:48:45 +0100

    Break out record

--- recorder/src/main.py
+++ recorder/src/main.py
@@ -1,8 +1,5 @@
 from contextlib import asynccontextmanager
-from pathlib import Path
 from datetime import datetime
-from subprocess import CalledProcessError
-from tempfile import NamedTemporaryFile
 import logging
 from typing import Annotated, Callable, Sequence
 
@@ -12,7 +9,6 @@ import httpx
 from fastapi import Depends, FastAPI, HTTPException, Query, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import ffmpeg
 from pydantic import BaseModel
 
 from common.auth import firebase
@@ -20,8 +16,8 @@ from common.auth import google
 from sqlalchemy import create_engine
 from sqlmodel import Session
 
-from gcs import upload_to_gcs
 from recorder.src.db import queries
+from recorder.src.record import record_and_save
 from recorder.src.timelapse.create_save import create_and_save_timelapse
 import recorder.src.db.models as models
 
@@ -234,7 +230,7 @@ def record(
     ]
     for device, url in devices:
         try:
-            _record_and_save(
+            record_and_save(
                 str(request.base_url),
                 url,
                 device,
@@ -245,67 +241,6 @@ def record(
         except RuntimeError as e:
             logging.error(f"Failed to record for device {device.name}: {e}")
     return {}
-
-
-def _record_and_save(
-    base_url: str,
-    device_url: str,
-    device: models.Device,
-    recording_dir: str,
-    duration: int,
-    session: Session,
-) -> None:
-    output_path = f"{recording_dir}/{device.name}/{datetime.now().isoformat()}.mp4"
-    if recording_dir.startswith("gs://"):
-        with NamedTemporaryFile(suffix=".mp4") as temp_file:
-            _record(device_url, device.name, temp_file.name, duration)
-            url = upload_to_gcs(temp_file.name, output_path)
-    else:
-        _record(device_url, device.name, output_path, duration)
-        url = _get_local_recording_url(base_url, recording_dir, Path(output_path))
-    logging.info(f"Saved recording for {device.name} to {url}")
-    recording = models.Recording(
-        device_id=device.id,
-        url=url,
-    )
-    session.add(recording)
-    session.commit()
-    session.refresh(recording)
-
-
-def _record(device_url: str, device: str, output_path: str, duration: int) -> None:
-    logging.info(f"Saving recording for {device}")
-    start_url = f"{device_url}/start"
-    start_response = httpx.get(
-        start_url, params={"bitrate": 10000000, "framerate": 30}, timeout=60.0
-    )
-    try:
-        start_response.raise_for_status()
-    except httpx.HTTPStatusError as e:
-        logging.error(f"Failed to start recording for {device}: {e}")
-        raise RuntimeError(f"Failed to start recording for {device}: {e}") from e
-    playlist_path = start_response.json()["playlist"]
-    playlist_url = f"{device_url}{playlist_path}"
-    Path(output_path).parent.mkdir(parents=True, exist_ok=True)
-    input_ = ffmpeg.input(playlist_url, t=duration)
-    output = ffmpeg.output(
-        input_,
-        output_path,
-        vcodec="libx264",
-        acodec="aac",
-        movflags="+faststart",
-        profile="main",
-        pix_fmt="yuv420p",
-    )
-    try:
-        ffmpeg.run(output, overwrite_output=True, quiet=True)
-        logging.info(f"Finished recording video for {device} to {output_path}")
-    except CalledProcessError as e:
-        logging.error(
-            f"Failed to record video for {device}: {e}\n"
-            f"stderr: {e.stderr}\n"
-            f"stdout: {e.stdout}"
-        )
 
 
 @app.get("/recording/{path:path}")
@@ -351,7 +286,3 @@ def create_timelapse(
             settings.recording_dir,
             session,
         )
-
-
-def _get_local_recording_url(url: str, recording_dir: str, path: Path) -> str:
-    return f"{url}recording/{path.relative_to(recording_dir)}"
---
+++ recorder/src/record.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from tempfile import NamedTemporaryFile
+import ffmpeg
+import httpx
+
+
+import logging
+from pathlib import Path
+from subprocess import CalledProcessError
+
+import recorder.src.db.models as models
+from recorder.src.gcs import upload_to_gcs
+
+from sqlmodel import Session
+
+
+def record_and_save(
+    base_url: str,
+    device_url: str,
+    device: models.Device,
+    recording_dir: str,
+    duration: int,
+    session: Session,
+) -> None:
+    output_path = f"{recording_dir}/{device.name}/{datetime.now().isoformat()}.mp4"
+    if recording_dir.startswith("gs://"):
+        with NamedTemporaryFile(suffix=".mp4") as temp_file:
+            _record(device_url, device.name, temp_file.name, duration)
+            url = upload_to_gcs(temp_file.name, output_path)
+    else:
+        _record(device_url, device.name, output_path, duration)
+        url = _get_local_recording_url(base_url, recording_dir, Path(output_path))
+    logging.info(f"Saved recording for {device.name} to {url}")
+    recording = models.Recording(
+        device_id=device.id,
+        url=url,
+    )
+    session.add(recording)
+    session.commit()
+    session.refresh(recording)
+
+
+def _record(device_url: str, device: str, output_path: str, duration: int) -> None:
+    logging.info(f"Saving recording for {device}")
+    start_url = f"{device_url}/start"
+    start_response = httpx.get(
+        start_url, params={"bitrate": 10000000, "framerate": 30}, timeout=60.0
+    )
+    try:
+        start_response.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        logging.error(f"Failed to start recording for {device}: {e}")
+        raise RuntimeError(f"Failed to start recording for {device}: {e}") from e
+    playlist_path = start_response.json()["playlist"]
+    playlist_url = f"{device_url}{playlist_path}"
+    Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+    input_ = ffmpeg.input(playlist_url, t=duration)
+    output = ffmpeg.output(
+        input_,
+        output_path,
+        vcodec="libx264",
+        acodec="aac",
+        movflags="+faststart",
+        profile="main",
+        pix_fmt="yuv420p",
+    )
+    try:
+        ffmpeg.run(output, overwrite_output=True, quiet=True)
+        logging.info(f"Finished recording video for {device} to {output_path}")
+    except CalledProcessError as e:
+        logging.error(
+            f"Failed to record video for {device}: {e}\n"
+            f"stderr: {e.stderr}\n"
+            f"stdout: {e.stdout}"
+        )
+
+
+def _get_local_recording_url(url: str, recording_dir: str, path: Path) -> str:
+    return f"{url}recording/{path.relative_to(recording_dir)}"
