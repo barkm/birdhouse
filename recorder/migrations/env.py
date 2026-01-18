e355c291ff8a0b6fc849a0944424b0158fcf22ba
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-18 19:29:43 +0100

    Fix models reference

--- recorder/migrations/env.py
+++ recorder/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from sqlmodel import SQLModel
-import recorder.src.db.models  # noqa: F401
+import src.db.models  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
